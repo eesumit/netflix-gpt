2f347de1147d753062ad80c766eec4332453c900
sumit <yessumitsingh007@gmail.com>
2025-02-15 23:32:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div className="bg-red-500 font-extrabold text-7xl m-10 p-10 flex items-center justify-center rounded-2xl" >Hello World.</div>
+  )
+}
+
+export default App
